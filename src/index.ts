9c4b128545fac458a873461058a451cf94ad3402
Kenny <kablesgaming@gmail.com>
2026-09-15 23:29:13 -0400

    Connect application to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import mongoose from "mongoose";
 import router from "./routes/index.js";
 import { logger } from "./middleware/logger.js";
 import { notFoundHandler, errorHandler } from "./middleware/error.js";
@@ -29,6 +30,12 @@ app.use(router);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+  });
