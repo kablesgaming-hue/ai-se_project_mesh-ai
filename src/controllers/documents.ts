a2aa3ac2255609beac53eeb85d06d486c695b76a
Kenny <kablesgaming@gmail.com>
2026-09-16 00:06:27 -0400

    Add document chunking and embeddings

--- src/controllers/documents.ts
+++ src/controllers/documents.ts
@@ -1,5 +1,10 @@
 import type { Request, Response } from "express";
+import { readFile } from "node:fs/promises";
+import { PDFParse } from "pdf-parse";
 import Document from "../models/document.js";
+import Chunk from "../models/chunk.js";
+import { chunkText } from "../utils/chunk.js";
+import { createEmbedding } from "../utils/embeddings.js";
 
 export const uploadDocument = async (
   req: Request,
@@ -23,6 +28,25 @@ export const uploadDocument = async (
     fileName: req.file.filename,
     userId,
   });
+
+  const buffer = await readFile(req.file.path);
+  const parser = new PDFParse({ data: buffer });
+  const result = await parser.getText();
+  await parser.destroy();
+
+  const chunks = chunkText(result.text);
+
+  await Promise.all(
+    chunks.map(async (text) => {
+      const embedding = await createEmbedding(text);
+
+      return Chunk.create({
+        documentId: document._id,
+        text,
+        embedding,
+      });
+    }),
+  );
 
   res.status(201).json({
     success: true,
