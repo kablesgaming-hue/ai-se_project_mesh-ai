a496d7070666452cd1e101704721383907061194
Kenny <kablesgaming@gmail.com>
2026-09-15 23:25:18 -0400

    Install Part 2 dependencies and configure environment

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import router from "./routes/index.js";
 import { logger } from "./middleware/logger.js";
 import { notFoundHandler, errorHandler } from "./middleware/error.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(logger);
